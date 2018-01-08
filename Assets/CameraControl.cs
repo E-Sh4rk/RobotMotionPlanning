1481f9d73de897feff32de83adb7f2ffbde33e39
Mickael <esh4rk@gmail.com>
2018-01-08 22:04:35 +0100

    camera

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -6,11 +6,31 @@ public class CameraControl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+        goToPos();
 	}
 
     public float velocity = 10.0f;
-	
+    public int currentPos = 0;
+
+    void goToPos()
+    {
+        switch (currentPos)
+        {
+            case 0:
+                transform.position = new Vector3(0,25,0);
+                transform.rotation = Quaternion.Euler(90,0,0);
+                break;
+            case 1:
+                transform.position = new Vector3(-15, 15, 15);
+                transform.rotation = Quaternion.Euler(45, 135, 0);
+                break;
+            case 2:
+                transform.position = new Vector3(15, 15, -15);
+                transform.rotation = Quaternion.Euler(145, 135, 180);
+                break;
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
         if (!Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.RightControl))
@@ -24,6 +44,10 @@ public class CameraControl : MonoBehaviour {
             if (Input.GetKey(KeyCode.DownArrow))
                 this.transform.Translate(Vector3.down * velocity * Time.fixedDeltaTime);
         }
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentPos = (currentPos + 1) % 3;
+            goToPos();
+        }
     }
 }
